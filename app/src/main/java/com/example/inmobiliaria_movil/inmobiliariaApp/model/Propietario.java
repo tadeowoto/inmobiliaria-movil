55ef9d67963dfcccbb4194f1a00d8f81a216dd76
Tadeo Wotoszyn <tadeowotoszyn@MacBook-Pro-de-Tadeo.local>
2025-10-09 13:44:36 -0300

    feat: agregue la entidad del propietario

--- app/src/main/java/com/example/inmobiliaria_movil/inmobiliariaApp/model/Propietario.java
+++ app/src/main/java/com/example/inmobiliaria_movil/inmobiliariaApp/model/Propietario.java
@@ -2,6 +2,80 @@ package com.example.inmobiliaria_movil.inmobiliariaApp.model;
 
 public class Propietario {
 
+    private int idPropietario;
+    private String nombre;
+    private String apellido;
+    private String dni;
+    private String telefono;
+    private String email;
+    private String clave;
+
+    // Constructor, getters y setters
 
 
+    public Propietario(int idPropietario, String nombre, String apellido, String dni, String telefono, String email, String clave) {
+        this.idPropietario = idPropietario;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.dni = dni;
+        this.telefono = telefono;
+        this.email = email;
+        this.clave = clave;
+    }
+
+    public int getIdPropietario() {
+        return idPropietario;
+    }
+
+    public void setIdPropietario(int idPropietario) {
+        this.idPropietario = idPropietario;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getClave() {
+        return clave;
+    }
+
+    public void setClave(String clave) {
+        this.clave = clave;
+    }
 }
