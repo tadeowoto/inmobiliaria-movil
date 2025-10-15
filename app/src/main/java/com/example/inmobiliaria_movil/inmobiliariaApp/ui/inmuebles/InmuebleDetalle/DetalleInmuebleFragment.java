21cd86426841084d0a7b94bc0904d461aeeb9df5
Tadeo Wotoszyn <tadeowotoszyn@MacBook-Pro-de-Tadeo.local>
2025-10-15 13:44:04 -0300

    feat: agregue vistas vacias de detalle inmueble y boton para el card_inmueble

---
+++ app/src/main/java/com/example/inmobiliaria_movil/inmobiliariaApp/ui/inmuebles/InmuebleDetalle/DetalleInmuebleFragment.java
@@ -0,0 +1,38 @@
+package com.example.inmobiliaria_movil.inmobiliariaApp.ui.inmuebles.InmuebleDetalle;
+
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.inmobiliaria_movil.R;
+
+public class DetalleInmuebleFragment extends Fragment {
+
+    private DetalleInmuebleViewModel mViewModel;
+
+    public static DetalleInmuebleFragment newInstance() {
+        return new DetalleInmuebleFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_detalle_inmueble, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(DetalleInmuebleViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/example/inmobiliaria_movil/inmobiliariaApp/ui/inmuebles/InmuebleDetalle/DetalleInmuebleViewModel.java
@@ -0,0 +1,7 @@
+package com.example.inmobiliaria_movil.inmobiliariaApp.ui.inmuebles.InmuebleDetalle;
+
+import androidx.lifecycle.ViewModel;
+
+public class DetalleInmuebleViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
